df9c38d4f95d6ef8da22508cff07a920d795e4fe
Sonia Chaboud <sonia.chaboud@gmail.com>
2023-09-15 00:53:47 +0200

    yesss skeletonnn!

--- src/Components/RecipeCards.jsx
+++ src/Components/RecipeCards.jsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom";
 import RecipeInstructions from "./RecipeInstructions";
-export default function RecipeCards({recipes}){
-    console.log(recipes);
 
-    return(
-        <>
-        {recipes && recipes.map(recipe =>(
-            <div className="recipecard" key={recipe.sys.id}>
-                <Link to={`/${recipe.sys.id}`}>
-    <h1>{recipe.fields.recipeName}</h1>
-    </Link>
-    <img src= {recipe.fields.picture.fields.file.url}/>
-  </div>
-))}
+export default function RecipeCards({ recipes }) {
+  console.log("recipe cards up", recipes);
+
+  return (
+    <>
+      {recipes &&
+        recipes.map((recipe) => (
+          <div className="recipecard" key={recipe.sys.id}>
+            <Link to={`/recipeinstructions/${recipe.sys.id}`}>
+              <h1>name:{recipe.fields.recipeName}</h1>
+            </Link>
+            <img src={recipe.fields.picture.fields.file.url} />
+          </div>
+        ))}
     </>
-    );
+  );
 }
