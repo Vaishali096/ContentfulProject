424ad8bc6067d03c6f77cb923c8836bf33244b34
Vaishali096 <vaishalivenkateshwaran@gmail.com>
2023-09-19 17:17:04 +0200

    last edit

--- src/Components/RecipeInstructions.jsx
+++ src/Components/RecipeInstructions.jsx
@@ -1,4 +1,5 @@
 import { useParams, useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
 export default function RecipeInstructions({ recipes }) {
   const navigate = useNavigate();
   const { id } = useParams();
@@ -8,7 +9,8 @@ export default function RecipeInstructions({ recipes }) {
   return (
     <>
       <div className="singleMember">
-        <h2>{oneRecipe && oneRecipe.fields.recipeName}</h2>
+      <Navbar />
+        <h3>{oneRecipe && oneRecipe.fields.recipeName}</h3>
         <div className= "singlerecipe">
         <img
           src={`/${oneRecipe && oneRecipe.fields.picture.fields.file.url}`}
@@ -17,12 +19,14 @@ export default function RecipeInstructions({ recipes }) {
           <br />
           <br />
           {oneRecipe && oneRecipe.fields.ingredients}</p>
-        </div>
+          </div>
+         <div className="instruct">
         <p>Instructions:</p>
         <p>{oneRecipe && oneRecipe.fields.instruction}</p>
         <button onClick={() => navigate("/RecipeCards")}>
           Back to Recipes
         </button>
+        </div>
       </div>
     </>
   );
